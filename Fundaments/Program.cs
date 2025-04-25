09924f1e08274f1eea6188097f27c44342ee5653
sebas777 <sebastiancast06@hotmail.com>
2025-04-25 15:45:01 -0500

    tipos de datos

--- Fundaments/Program.cs
+++ Fundaments/Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+namespace MyNamespace
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 13;
+            int num2 = 5;
+            int sum = num1+ num2;
+            Console.WriteLine("La suma de " + num1 +" mas "+ num2 + " da como resultado : " + sum);
+            
+
+            double d1 = 0.5;
+            double d2 = 0.5;
+            double sum1 = d1 + d2 + num1;
+            Console.WriteLine("La suma de  " + d1 + " mas " + d2 + " mas " + num1 + " da como resultado " + sum1);
+            Console.Read();
+        }
+    }   
+}
---
+++ String/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Strings 
+{
+    class Strings 
+    {
+        static void Main(string[] args)
+        {
+            string miNombre = "Isabella";
+            string mensaje = " la mujersita mas linda es " +  miNombre;
+            Console.WriteLine(mensaje);
+
+            string mayusculas = mensaje.ToUpper();
+            Console.WriteLine(mayusculas);
+            
+            string minusculas = mensaje.ToLower();
+            Console.WriteLine(minusculas);
+        }
+        
+    }
+    
+}
+
